ad87a0e35e9c084e69c0c095cfabf7647aa1dc8a
Vontha Chan <89168934+vonthachan@users.noreply.github.com>
2026-09-07 23:39:23 -0500

    FSO 2.2 done

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,5 +1,8 @@
 import Part from "./Part";
 const Content = ({ parts }) => {
+  const total = parts.reduce((sum, part) => {
+    return sum + part.exercises;
+  }, 0);
   return (
     <div>
       {parts.map((part) => (
@@ -7,6 +10,7 @@ const Content = ({ parts }) => {
           <Part name={part.name} exercises={part.exercises} />
         </li>
       ))}
+      <li>total of {total} exericises</li>
     </div>
   );
 };
