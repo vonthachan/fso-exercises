dbe6ca1d291397e3bfa01b7981eb5e1bfc20fac7
Vontha Chan <89168934+vonthachan@users.noreply.github.com>
2026-09-16 22:19:32 -0500

    restarting 2.6 for practice

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,50 +1,40 @@
 import { useState } from "react";
-import Person from "./components/Person";
-
+//Goal add names to phonebook and display
 const App = () => {
-  //Array of names in the phonebook
   const [persons, setPersons] = useState([
     { name: "Arto Hellas" },
-    { name: "Hello" },
+    { name: "asdasd" },
+    { name: "ruby" },
   ]);
-  //The text of the input (controlling form input element)
   const [newName, setNewName] = useState("");
 
-  //Handles adding the name to persons
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    if (persons.some((person) => person.name === newName)) return;
-    setPersons(persons.concat({ name: newName }));
-    setNewName("");
-  };
-
-  //sets new name as the input whenever it is changed
   const handleNameChange = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value);
   };
 
+  const addPerson = (event) => {
+    event.preventDefault();
+    console.log("add button clicked");
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
-        <div>{newName}</div>
-        <h2>Numbers</h2>
-        <ul style={{ margin: 0, padding: 0 }}>
-          {persons.map((person) => (
-            <li style={{ listStyle: "none", margin: 0, paddingLeft: 0 }}>
-              <Person key={person.name} name={person.name} />
-            </li>
-          ))}
-        </ul>
       </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li>{person.name}</li>
+        ))}
+      </ul>
       ...
     </div>
   );
