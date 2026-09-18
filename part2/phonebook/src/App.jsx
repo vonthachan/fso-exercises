afc19021ac6eed67dc41fd1f817591f871110116
Vontha Chan <89168934+vonthachan@users.noreply.github.com>
2026-09-18 11:41:32 -0500

    FSO 2.9 Done! Filtering added

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,10 +4,14 @@ import Person from "./components/Person";
 //Goal add names to phonebook and display
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "123222" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setNewFilter] = useState("");
 
   //Name Stuff
   const handleNameChange = (event) => {
@@ -44,9 +48,26 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  // Filtering
+  // Steps get filter text> make a new array with matching filter persons > map the filtered people
+  const filteredPersons = filter === "" ? persons : persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  //Sets filter text
+  const handleFilter = (event) => {
+    console.log("Filter: " + event.target.value);
+    // console.log("Filtered persons: " + filteredPersons)
+    setNewFilter(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={handleFilter} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div style={{ listStyle: "none", margin: 0, padding: 0 }}>
           <div>
@@ -62,7 +83,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul style={{ listStyle: "none", padding: 0 }}>
-        {persons.map((person) => (
+        {filteredPersons.map((person) => (
           <Person key={person.name} name={person.name} number={person.number} />
         ))}
       </ul>
